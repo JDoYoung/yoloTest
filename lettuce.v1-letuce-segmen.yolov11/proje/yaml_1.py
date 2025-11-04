0807fe5fd2af185e4b21de5f42f31885358a4f33
JDoYoung <airwalk6346@gmail.com>
2025-11-04 13:51:50 +0900

    feat: 상추 모델 학습 준비 및 실행 코드

---
+++ lettuce.v1-letuce-segmen.yolov11/proje/yaml_1.py
@@ -0,0 +1,44 @@
+import yaml
+from ultralytics import YOLO
+
+# 1. data.yaml 경로
+yaml_path = '/home/aa/lettuce.v1-letuce-segmen.yolov11/data.yaml'
+
+# 2. data.yaml 생성
+data = {
+    'train': '/home/aa/yoloTest/lettuce.v1-letuce-segmen.yolov11/train/images',
+    'val': '/home/aa/yoloTest/lettuce.v1-letuce-segmen.yolov11/valid/images',
+    'test': '/home/aa/yoloTest/lettuce.v1-letuce-segmen.yolov11/test/images',
+    'names': ['Baby', 'Mature', 'Bad'],
+    'nc': 3
+}
+
+with open(yaml_path, 'w') as f:
+    yaml.dump(data, f)
+
+# 확인
+with open(yaml_path, 'r') as f:
+    print(yaml.safe_load(f))
+
+# 3. 모델 로드
+model = YOLO('yolo11n.pt')
+
+# 4. 클래스 정보 출력
+print("Before Training:")
+print(type(model.names), len(model.names))
+print(model.names)
+
+# 5. 학습 시작
+model.train(
+    data=yaml_path,
+    epochs=30,
+    patience=5,
+    imgsz=416,
+    project='/home/aa/yoloTest/lettuce.v1-letuce-segmen.yolov11',  # ⬅️ 결과 저장 위치
+    name='train_result'                                     # 폴더명: train_result
+)
+
+# 6. 학습 후 클래스 정보 출력
+print("After Training:")
+print(type(model.names), len(model.names))
+print(model.names)
